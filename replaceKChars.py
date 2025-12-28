73cf89ef682bee88601ae969f63cee7ff9f1750a
alex1bondoc <alex1bondoc@gmail.com>
2025-12-28 19:28:56 +0200

    replaceKChars.py

--- replaceKChars.py
+++ replaceKChars.py
@@ -1,21 +1,15 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        uniq = set(s)
-        max = 0
-        for u in uniq:
-            cnt = 0
-            l = 0
-            r = -1
-            for r in range(len(s)):
-                if s[r] != u:
-                    if cnt < k:
-                        cnt += 1
-                    else :
-                        while s[l] == u:
-                            l += 1
-                        l += 1
-                if r - l + 1 > max:
-                    max = r - l + 1
-                
-        return max
-        
+        sol = 0
+        count  = {}
+        l = 0
+        maxi = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r],0)
+            maxi = max(maxi, count[s[r]])
+            while (r - l + 1) - maxi > k:
+                count[s[l]] -= 1
+                l += 1
+            sol = max(sol, r - l + 1)
+
+        return sol       
