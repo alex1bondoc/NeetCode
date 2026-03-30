85596214b6bcedda5e9bdfd962129c7752ef4485
alex1bondoc <alex1bondoc@gmail.com>
2026-03-30 18:40:10 +0300

    Histogram recap

--- Histogram.py
+++ Histogram.py
@@ -1,4 +1,15 @@
-class Solution:
+cclass Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        stack = []
+        res = 0
+        for i in range(n + 1):
+            while stack and (i == n or heights[stack[-1]]>= heights[i]):
+                h = heights[stack.pop()]
+                w = i if not stack else  i - stack[-1] - 1
+                res = max(res, h * w)
+            stack.append(i)
+        return reslass Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         stack = []
         res = 0
